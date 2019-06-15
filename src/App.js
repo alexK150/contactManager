1042f3a58e5898e2d5c62bca74bde30f33c67a5a
Alex <akuzin20@gmail.com>
2019-06-15 21:05:52 +0300

    commit v2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Contact from './components/Contact';
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component{
@@ -7,8 +8,8 @@ class App extends Component{
   render() {
       return (
           <div className="App">
-              <h1>The App Component</h1>
-            <Contact />
+            <Header />
+            <Contact name="John Doe" email="qeeerrr@qwee" phone="90987-6665-55"/>
           </div>
       );
   }
